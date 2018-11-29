01ab1703ac776a56bbf4fdee2eb7a4036b70c9d7
root <root@DLC-PROJET.istic.univ-rennes1.fr>
2018-11-29 22:10:01 +0100

    recherche film + delete useless files

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function search(){
+	$.post("/searchMovie", JSON.stringify({movie: $("#searchMovie").val()}), function (data, status) {
+		console.log(data);
+	}).fail(function(){ 
+		alert('something went wrong please try again');
+	});
+}
 
--- server.js
+++ server.js
@@ -1,27 +1,71 @@
 const	http = require('http'),
-		fs = require('fs');
+			mysql = require('mysql'),
+			fs = require('fs');
 
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'user',
+  password : 'mypassword',
+  database : 'db'
+});
+
+connection.connect();
 
 http.createServer(function (req, res) {
-  if (req.url == '/') {
-    fs.readFile('index.html', function (err, data) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url == '/style.css') {
-    fs.readFile('style.css', function (err, data) {
-      res.writeHead(200, { 'Content-Type': 'text/css' });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url == '/script.js') {
-    fs.readFile('script.js', function (err, data) {
-      res.writeHead(200, { 'Content-Type': 'text/js' });
-      res.write(data);
-      res.end();
-    });
-  } else {
-	  res.end();
-  }
+	
+	if(req.method === "POST"){
+			var body = "";
+			req.on("data", function (data) {
+					body += data;
+			});
+			req.on("end", function(){
+				try{
+					const json = JSON.parse(body);
+					if(req.url === "/searchMovie"){
+						connection.query('SELECT * FROM `movies` WHERE `originalTitle` like ? LIMIT 100', json['movie']+"%", function (error, results, fields) {
+							if(error){
+								res.writeHead(500, "Internal Server Error", {"Content-Type": "text/plain"});
+								res.end();
+							}else{
+								res.writeHead(200, "OK", {"Content-Type": "application/json"});
+								res.write(JSON.stringify(results));
+								res.end();								
+							}
+						});
+					}else if(req.url === "/searchPerson"){
+						throw "unknown";
+					}else{
+						throw "unknown";
+					}
+				}catch(e){
+				console.log(e);
+					res.writeHead(400, "Bad Request", {"Content-Type": "text/plain"});
+					res.end();
+				}
+			});
+	}else{
+		if (req.url == '/') {
+			fs.readFile('index.html', function (err, data) {
+				res.writeHead(200, "OK", { 'Content-Type': 'text/html' });
+				res.write(data);
+				res.end();
+			});
+		} else if (req.url == '/style.css') {
+			fs.readFile('style.css', function (err, data) {
+				res.writeHead(200, "OK", { 'Content-Type': 'text/css' });
+				res.write(data);
+				res.end();
+			});
+		} else if (req.url == '/script.js') {
+			fs.readFile('script.js', function (err, data) {
+				res.writeHead(200, "OK", { 'Content-Type': 'text/js' });
+				res.write(data);
+				res.end();
+			});
+		} else {
+				res.writeHead(400, "Bad Request", {"Content-Type": "text/plain"});
+				res.end();
+		}
+	}
 }).listen(8080);
+
