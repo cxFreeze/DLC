e51f3f2d3a2fcf8b5e58abe23915033afc78d97d
Corentin Roche <corentin.roche@etudiant.univ-rennes1.fr>
2018-12-14 13:24:18 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,26 +1,18 @@
 var supertest = require("supertest");
 var should = require("should");
+var expect = require("expect");
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:4000");
+var server = supertest.agent("http://localhost:80");
 
-// UNIT test begin
-
-describe("SAMPLE unit test",function(){
-
-  // #1 should return home page
-  it("should return home page",function(done){
-    // calling home page
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(200) // THis is HTTP response
-    .end(function(err,res){
-      // HTTP status should be 200
+server
+   .get("/")
+   .expect("Content-type",/text/)
+   .expect(200) // THis is HTTP response
+   .end(function(err,res){
+      console.log(err,res);
       res.status.should.equal(200);
       done();
     });
-  });
-
 });
