cb54e3e61ee3a8c7c2455716e06e822e9d181c0b
root <root@dlc-projet2.istic.univ-rennes1.fr>
2018-12-14 13:29:08 +0100

    osef

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 var supertest = require("supertest");
 var should = require("should");
-
+var describe = require("describe");
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:4000");
-
+var server = supertest.agent("http://localhost");
+console.log('test');
 // UNIT test begin
 
 describe("SAMPLE unit test",function(){
-
   // #1 should return home page
+  console.log('test2')
   it("should return home page",function(done){
     // calling home page
     server
@@ -17,6 +17,7 @@ describe("SAMPLE unit test",function(){
     .expect("Content-type",/text/)
     .expect(200) // THis is HTTP response
     .end(function(err,res){
+      console.log(err,res);
       // HTTP status should be 200
       res.status.should.equal(200);
       done();
