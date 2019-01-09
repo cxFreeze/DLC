a8990d820aca98b02d483b3191ce4e9dcc9634ff
root <root@dlc-projet2.istic.univ-rennes1.fr>
2019-01-09 13:26:11 +0100

    test

--- launch.sh
+++ launch.sh
@@ -1,7 +1,6 @@
 docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
-docker system prune -f --volumes
+docker rmi $(docker images |grep 'dlc/projet')
 docker build -t dlc/projet .
 docker run -p 8000:8000 -d dlc/projet
 docker run -p 8001:8000 -d dlc/projet
