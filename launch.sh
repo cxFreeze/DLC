f1ad2bfcfeda2f858f0e8e5d231250706ff071d7
root <root@dlc-projet2.istic.univ-rennes1.fr>
2019-01-09 12:46:09 +0100

    lance 2 container docker

---
+++ launch.sh
@@ -0,0 +1,8 @@
+docker stop $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+docker system prune -f --volumes
+docker build -t dlc/projet .
+docker run -p 8000:8000 -d dlc/projet
+docker run -p 8001:8000 -d dlc/projet
+npm test
