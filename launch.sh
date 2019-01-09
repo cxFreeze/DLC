ccdd43a8af377d799d5343377fb76d2d5b2a62c9
root <root@dlc-projet2.istic.univ-rennes1.fr>
2019-01-09 14:24:24 +0100

    param

--- launch.sh
+++ launch.sh
@@ -2,6 +2,12 @@ docker stop $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q 'dlc/projet')
 docker build -t dlc/projet .
-docker run -p 8000:8000 -d dlc/projet
-docker run -p 8001:8000 -d dlc/projet
+
+counter=0
+while [ $counter -lt $1 ]
+do
+	docker run --cpus="0.2" --memory="100m" -p 800$counter:8000 -d dlc/projet
+	((counter++))
+done
+
 npm test
