c0214f7f7ceb170e535f22fb54233116c33a56c4
Corentin ROCHE <corentinr@sxb.incotec.fr>
2019-01-11 15:30:32 +0100

    fix

--- launch.sh
+++ launch.sh
@@ -10,4 +10,4 @@ do
 	docker run --cpus="0.1" --memory="100m" -p 800$counter:8000 -d dlc/projet
 	((counter++))
 done
-docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=9000:8080 --detach=true --name=cadvisor google/cadvisor:latest
+docker run --cpus="0.1" --memory="100m" --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=9000:8080 --detach=true --name=cadvisor google/cadvisor:latest
