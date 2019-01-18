bf72f0fcc13df0910c3308d4bc366eb206fc8ff5
Karim <15703451+kiko35@users.noreply.github.com>
2019-01-18 15:31:20 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ docker build -t dlc/projet .
 counter=0
 while [ $counter -lt $1 ]
 do
-	docker run --cpus="0.1" --memory="100m" -p 800$counter:8000 -d dlc/projet
+	docker run --cpus="0.1" --memory="50m" -p 800$counter:8000 -d dlc/projet
 	((counter++))
 done
 docker run --cpus="0.1" --memory="100m" --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=9000:8080 --detach=true --name=cadvisor google/cadvisor:latest
