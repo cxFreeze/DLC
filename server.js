cd0cd30ec8fa63a4a13a5ee7bc1be7de66822ac8
Karim <15703451+kiko35@users.noreply.github.com>
2019-01-31 14:10:46 +0100

    release pool

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.post('/app/searchMovie', function (req, res) {
 		}
 		connection.query('SELECT movies.*, GROUP_CONCAT(CONCAT(persons.nconst, "$", `primaryName`) ) AS cast FROM (SELECT * FROM movies WHERE `originalTitle` like ? ORDER BY CAST(`numVotes` AS UNSIGNED) DESC LIMIT 10) AS movies LEFT JOIN `principals` ON movies.tconst=principals.tconst LEFT JOIN `persons` ON principals.nconst=persons.nconst WHERE characters !="" OR characters IS NULL GROUP BY movies.tconst ORDER BY CAST(`numVotes` AS UNSIGNED) DESC', "%"+json['movie']+"%", 
 		(error, results, fields) => {
+			connection.release();
 			error ? res.sendStatus(500) : res.json(results);
 			res.end();								
 		});
@@ -59,6 +60,7 @@ app.post('/app/getMovieDetails', function (req, res) {
 		}
 		connection.query('SELECT movies.*, persons.nconst, `primaryName`, `job`, `characters` FROM (SELECT * FROM movies WHERE `tconst` = ?) AS movies LEFT JOIN `principals` ON movies.tconst=principals.tconst LEFT JOIN `persons` ON principals.nconst=persons.nconst', json['movie'], 
 		(error, results, fields) => {
+			connection.release();
 			error ? res.sendStatus(500) : res.json(results);
 			res.end();
 		});
@@ -75,6 +77,7 @@ app.post('/app/addMovie', function (req, res) {
 		}
 		connection.query('INSERT INTO movies(tconst, originalTitle, startYear, averageRating, runtimeMinutes, genres) values(?,?,?,?,?,?)', [crypto.randomBytes(8).toString("hex"), json['title'], json['year'],json['note'],json['time'],json['genres']], 
 		(error, results, fields) => {
+			connection.release();
 			error ? res.sendStatus(500) : res.send("OK");
 			res.end();
 		});
@@ -92,6 +95,7 @@ app.post('/app/getPersonDetails', function (req, res) {
 		connection.query('SELECT persons.* FROM persons WHERE nconst=?', json['person'],
 		(error, results, fields) => {
 			if(error){
+				connection.release();
 				res.sendStatus(500);
 				res.end();
 			}else{
@@ -99,25 +103,20 @@ app.post('/app/getPersonDetails', function (req, res) {
 				 tmp = results[0].knownForTitles.split(',')
 				}
 				else{
+					connection.release();
 					res.json(results);
 					res.end();
 					return
 				}
-				poolCluster.getConnection(function (err, connection) {
-					if(err){
-						res.sendStatus(500);
-						res.end();
-						return;
+				connection.query('SELECT tconst, originalTitle FROM movies WHERE tconst=? OR tconst=? OR tconst=? ', [tmp[1], tmp[2],tmp[3]],
+				(error, results2, fields) => {
+					if(!error){				
+						results[0].movieNames = results2
 					}
-					connection.query('SELECT tconst, originalTitle FROM movies WHERE tconst=? OR tconst=? OR tconst=? ', [tmp[1], tmp[2],tmp[3]],
-					(error, results2, fields) => {
-						if(!error){				
-							results[0].movieNames = results2
-						}
-						res.json(results);
-						res.end();	
-					});				
-				});				
+					connection.release();
+					res.json(results);
+					res.end();	
+				});								
 			}
 		});
 	});
